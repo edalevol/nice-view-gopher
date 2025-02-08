2c759b0d9f84caa5abbc48b9a3d5bd74f5cbe6e1
eivind <eivind.dalevold@gmail.com>
2025-02-08 23:08:09 +0100

    replace crystal with gopher. Add config option to set the number of animation repeats

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -2,27 +2,26 @@
 #include <zephyr/kernel.h>
 #include "animation.h"
 
-LV_IMG_DECLARE(crystal_01);
-LV_IMG_DECLARE(crystal_02);
-LV_IMG_DECLARE(crystal_03);
-LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
+LV_IMG_DECLARE(goph1);
+LV_IMG_DECLARE(goph2);
+LV_IMG_DECLARE(goph3);
+LV_IMG_DECLARE(goph4);
+LV_IMG_DECLARE(goph5);
+LV_IMG_DECLARE(goph6);
+LV_IMG_DECLARE(goph7);
+LV_IMG_DECLARE(goph8);
+// LV_IMG_DECLARE(crystal_09);
+// LV_IMG_DECLARE(crystal_10);
+// LV_IMG_DECLARE(crystal_11);
+// LV_IMG_DECLARE(crystal_12);
+// LV_IMG_DECLARE(crystal_13);
+// LV_IMG_DECLARE(crystal_14);
+// LV_IMG_DECLARE(crystal_15);
+// LV_IMG_DECLARE(crystal_16);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &goph1, &goph2, &goph3, &goph4, &goph5, &goph4,
+    &goph3, &goph6, &goph7, &goph6, &goph3, &goph2,
 };
 
 void draw_animation(lv_obj_t *canvas) {
@@ -30,10 +29,12 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 16);
+    lv_animimg_set_src(art, (const void **)anim_imgs, 12);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
-    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_set_repeat_count(art, CONFIG_NICE_VIEW_GEM_ANIMATION_REPEAT);
     lv_animimg_start(art);
+    lv_img_set_src(art, anim_imgs[0]);
+    
 #else
     lv_obj_t *art = lv_img_create(canvas);
 
@@ -44,5 +45,5 @@ void draw_animation(lv_obj_t *canvas) {
     lv_img_set_src(art, anim_imgs[random_index]);
 #endif
 
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, -65, 0);
 }
