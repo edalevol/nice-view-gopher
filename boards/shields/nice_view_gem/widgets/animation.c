19dc0f99fbf8a3f9d29bbc12d3e9286a505f003a
eivind <eivind.dalevold@gmail.com>
2025-02-19 20:20:55 +0100

    'randomly' start anim

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <zephyr/kernel.h>
 #include "animation.h"
+#include <zephyr/kernel.h>
+
+
 
 LV_IMG_DECLARE(goph1);
 LV_IMG_DECLARE(goph2);
@@ -30,6 +33,27 @@ const lv_img_dsc_t *anim_imgs[] = {
     &goph3, &goph2,
 };
 
+// #include <stdlib.h>
+// #include <lvgl.h>
+
+// Function to restart the animation
+static void restart_animation(lv_timer_t * timer) {
+    lv_obj_t * art = (lv_obj_t *)timer->user_data; // Correct way to get user data
+    lv_animimg_start(art); // Restart the animation
+
+    // Schedule the next restart
+    int delay = (rand() % 11 + 5) * 1000; // Random delay between 5s and 15s
+    lv_timer_reset(timer);
+    lv_timer_set_period(timer, delay);
+}
+
+// Function to create a timer for animation replay
+static void schedule_animation_restart(lv_obj_t * art) {
+    int delay = (rand() % 11 + 5) * 1000; // Random delay between 5s and 15s
+    lv_timer_t * timer = lv_timer_create(restart_animation, delay, art);
+    lv_timer_set_repeat_count(timer, -1); // Infinite repeats
+}
+
 void draw_animation(lv_obj_t *canvas) {
 #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
     lv_obj_t *art = lv_animimg_create(canvas);
@@ -39,8 +63,12 @@ void draw_animation(lv_obj_t *canvas) {
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, CONFIG_NICE_VIEW_GEM_ANIMATION_REPEAT);
     lv_animimg_start(art);
+
+    // Schedule animation restart after a random delay
+    schedule_animation_restart(art);
+
     lv_img_set_src(art, anim_imgs[0]);
-    
+
 #else
     lv_obj_t *art = lv_img_create(canvas);
 
